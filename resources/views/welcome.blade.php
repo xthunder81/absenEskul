d98bc1024ff3dfc9c61e70ba2a4c5e130d1820ae
xthunder81 <xthunder81@rocketmail.com>
2019-10-24 14:48:36 +0700

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,14 +4,15 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Absensi Eskul Siswa</title>
-
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
 
         <!-- Bootstrap CSS -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
 
+        <!-- Title -->
+        <title>Absensi Eskul Siswa</title>
+        
         <!-- Styles -->
         <style>
             html, body {
